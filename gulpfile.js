5bf5717c74789fd064c7ce89b52dec4008ab4b64
mr_rahimi@live.com <mr_rahimi@live.com>
2020-02-22 00:46:02 +0330

    .gitignore is now working

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,14 @@ var uglify = require('gulp-uglify');
 var minify = require('gulp-minify-css');
 var autoprefixer    = require('gulp-autoprefixer');
 
-gulp.task('sass', function () {
+gulp.task('sass', gulp.series(async function () {
     return gulp.src('src/scss/**/*.scss')
         .pipe(sass({ outputStyle: "compressed" }).on('error', sass.logError))
         .pipe(autoprefixer(['last 2 versions', 'ie 8', 'ie 9']))
         .pipe(gulp.dest('dist/css/'));
-});
+}));
  
-gulp.task('js', function () {
+gulp.task('js', gulp.series(async function () {
     gulp.src(['src/js/VestaDatePicker.persian.js', 'src/js/VestaDatePicker.hijri.js', 
               'src/js/VestaDatePicker.gregorian.js', 'src/js/VestaDatePicker.js'])
         .pipe(concat('VestaDatePicker.full.min.js'))
@@ -22,12 +22,12 @@ gulp.task('js', function () {
         .pipe(gulp.dest('dist/js/'));
 
     gulp.src('src/js/*.js')
-        .pipe(gulp.dest('dist/js/'))
-});
+        .pipe(gulp.dest('dist/js/'));
+}));
 
-gulp.task('sass:watch', function () {
+gulp.task('sass:watch', gulp.series(async function () {
     gulp.watch('src/scss/**/*.scss', ['sass']);
-});
+}));
 
 
-gulp.task('build', ['sass', 'js']);
+gulp.task('build', gulp.series( 'sass', 'js'));
